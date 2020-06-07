a5fbc11642bf5c461e99df7257b770b926318496
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2020-06-07 15:18:46 +0200

    work

--- 3DGame-GTA-Antwerpen/Assets/Scripts/Enemy.cs
+++ 3DGame-GTA-Antwerpen/Assets/Scripts/Enemy.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Animator animator;
     public Transform Player;
     private Rigidbody rigidbody;
+    public float InputX;
+    public float InputY;
     public float speed = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = this.gameObject.GetComponent<Animator>();
         rigidbody = this.GetComponent<Rigidbody>();
     }
 
@@ -22,6 +26,8 @@ public class Enemy : MonoBehaviour
     public void FixedUpdate()
     {
         Vector3 position = Vector3.MoveTowards(transform.position, Player.position, speed * Time.fixedDeltaTime);
+        animator.SetFloat("InputX", Player.position.x);
+        animator.SetFloat("InputY", Player.position.y);
         rigidbody.MovePosition(position);
         transform.LookAt(Player);
     }
