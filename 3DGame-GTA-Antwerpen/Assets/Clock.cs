965c86a8888425357f1e7435d8742e63073c67e9
Kerem <s100757@ap.be>
2020-06-09 23:04:15 +0200

    Revert "Revert "Revert "Revert "Merge branch 'master' into Schieten""""
    This reverts commit 8ce9ad537566bad828322a0d3b16d8c41e0c2c98.

---
+++ 3DGame-GTA-Antwerpen/Assets/Clock.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+
+public class Clock : MonoBehaviour
+{
+    private Text clock;
+    public GameObject directionLightObject;
+    private Light directionLight;
+
+    public int hour = 12, minute1 = 0, minute2 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = GetComponent<Text>();
+        directionLight = directionLightObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minute2++;
+        if (minute2 > 9)
+        {
+            minute2 = 0;
+            minute1++;
+        }
+        else if (minute1 > 5)
+        {
+            minute1 = 0;
+            hour++;
+        }
+        else if (hour > 23)
+        {
+            hour = 0;
+        }
+        Thread.Sleep(50);
+        clock.text = hour + ":" + minute1 + minute2;
+
+        if (directionLight)
+        {
+            if(hour >= 6 && hour < 18)
+            {
+                directionLight.color = Color.white;
+            }
+            else if(hour >= 18 && hour < 21)
+            {
+                directionLight.color = Color.yellow;
+            }
+            else if(hour >= 21 && hour < 24)
+            {
+                directionLight.color = Color.black;
+            }
+            else if(hour >= 0 && hour < 6)
+            {
+                directionLight.color = Color.black;
+            }
+        }
+    }
+}
---
+++ 3DGame-GTA-Antwerpen/Assets/Scripts/EnterCar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements.GraphView;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class EnterCar : MonoBehaviour
+{
+    public GameObject CarCam;
+    public GameObject PlayerCam;
+    public GameObject Player;
+    public GameObject ExitTrigger;
+    public GameObject Car;
+    public int TriggerCheck = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (TriggerCheck)
+        {
+            case 1:
+                if (Input.GetButtonDown("Action"))
+                {
+                    CarCam.SetActive(true);
+                    PlayerCam.SetActive(false);
+                    Player.SetActive(false);
+                    Car.GetComponent<CarController>().enabled = true ;
+                    Car.GetComponent<CarUserControl>().enabled = true;
+                    ExitTrigger.SetActive(true);
+                }
+                break;
+        }
+        
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        TriggerCheck = 1;
+        System.Console.WriteLine("Trigger = 1");
+    }
+    public void OnTriggerExit(Collider collider)
+    {
+        TriggerCheck = 0;
+        System.Console.WriteLine("Trigger = 0");
+    }
+}
