6398b3e32b399d8a28e84de2e817985df79a3383
Ajon Subedi <ajonsubedi@gmail.com>
2020-06-09 19:57:26 +0200

    Timer in progress

---
+++ 3DGame-GTA-Antwerpen/Assets/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timer;
+    private static System.Timers.Timer timer_t;
+    public int minute = 0, second1 = 0, second2 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer_t = new System.Timers.Timer();
+        //timer.inter
+        second2--;
+        if (second2 == 0)
+        {
+            second2 = 9;
+            second1--;
+        }
+        else if (second1 == 0)
+        {
+            second1 = 9;
+            minute--;
+        }
+    } 
+}
