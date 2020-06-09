d01cfe55cdfe052d71e6ce950688d0971947ac11
Ajon Subedi <ajonsubedi@gmail.com>
2020-06-09 15:35:44 +0200

    Player can get in and out of a car

---
+++ 3DGame-GTA-Antwerpen/Assets/Scripts/EnterCar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class EnterCar : MonoBehaviour
+{
+    public GameObject CarCam;
+    public GameObject PlayerCam;
+    public GameObject Player;
+    public GameObject ExitTrigger;
+    public GameObject Car;
+    public int TriggerCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (TriggerCheck)
+        {
+            case 1:
+                if (Input.GetButtonDown("Action"))
+                {
+                    CarCam.SetActive(true);
+                    PlayerCam.SetActive(false);
+                    Player.SetActive(false);
+                    Car.GetComponent<CarController>().enabled = true ;
+                    Car.GetComponent<CarUserControl>().enabled = true;
+                    ExitTrigger.SetActive(true);
+                }
+                break;
+        }
+        
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        TriggerCheck = 1;
+    }
+    public void OnTriggerExit(Collider collider)
+    {
+        TriggerCheck = 0;
+    }
+}
