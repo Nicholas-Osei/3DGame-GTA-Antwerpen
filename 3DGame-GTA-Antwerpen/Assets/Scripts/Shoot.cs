59a72a26eb0f7466befda82ad4a584d3921d47b1
Kerem <s100757@ap.be>
2020-06-09 07:02:18 +0200

    Sound and Effect added

--- 3DGame-GTA-Antwerpen/Assets/Scripts/Shoot.cs
+++ 3DGame-GTA-Antwerpen/Assets/Scripts/Shoot.cs
@@ -6,13 +6,27 @@ public class Shoot : MonoBehaviour
 {
 
     public float Damage = 5;
-    public int range = 100;
+    public int GunRange = 100;
 
     [SerializeField]
     [Range(0.05f,1f)]
     public float FireRate = 1;
 
     private KeyCode Mouse1 = KeyCode.Mouse0;
+
+
+    public GameObject[] ParticleExamples;
+    private int exampleIndex;
+
+    public bool orderedSpawns = true;
+    public float step = 1.0f;
+    public float range = 5.0f;
+    private float order = -5.0f;
+    private string randomSpawnsDelay = "0.5";
+
+    [SerializeField]
+    private AudioSource GunSound;
+
 
     private float timer;
     public Transform firePoint;
@@ -29,11 +43,17 @@ public class Shoot : MonoBehaviour
                 timer = 0f;
                 FireGun();
             }
+            
         }
     }
 
     void FireGun()
     {
+        GameObject particle = spawnParticle();
+        particle.transform.position = particle.transform.position;
+        GunSound.Play();
+        
+        
         Debug.DrawRay(firePoint.position, firePoint.forward * 300, Color.red, 2f);
         Ray ray = new Ray(firePoint.position, firePoint.forward);
         RaycastHit hitinfo;
@@ -42,5 +62,47 @@ public class Shoot : MonoBehaviour
         {
             Destroy(hitinfo.collider.gameObject);
         }
+    }
+
+    private GameObject spawnParticle()
+    {
+        GameObject particles = (GameObject)Instantiate(ParticleExamples[exampleIndex]);
+        particles.transform.parent = ParticleExamples[exampleIndex].transform.parent;
+        particles.transform.localPosition = ParticleExamples[exampleIndex].transform.localPosition;
+        particles.transform.localRotation = ParticleExamples[exampleIndex].transform.localRotation;
+
+        SetActiveCrossVersions(particles, true);
+
+        return particles;
+    }
+
+    void SetActiveCrossVersions(GameObject obj, bool active)
+    {
+        #if UNITY_3_5
+				obj.SetActiveRecursively(active);
+        #else
+        obj.SetActive(active);
+        for (int i = 0; i < obj.transform.childCount; i++)
+            obj.transform.GetChild(i).gameObject.SetActive(active);
+        #endif
+    }
+
+    IEnumerator RandomSpawnsCoroutine()
+    {
+
+    LOOP:
+        GameObject particles = spawnParticle();
+
+        if (orderedSpawns)
+        {
+            particles.transform.position = this.transform.position + new Vector3(order, particles.transform.position.y, 0);
+            order -= step;
+            if (order < -range) order = range;
+        }
+        else particles.transform.position = this.transform.position + new Vector3(Random.Range(-range, range), 0,Random.Range(-range, range)) + new Vector3(0, particles.transform.position.y, 0);
+
+        yield return new WaitForSeconds(float.Parse(randomSpawnsDelay));
+
+        goto LOOP;
     }
 }
